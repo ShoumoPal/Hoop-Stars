44e5d02d9cebca6176a57c32f6db262aef4c9cce
ShoumoPal <arka.pal21@gmail.com>
2023-11-30 21:06:21 +0530

    Added Sounds

--- Assets/Scripts/Ring/RingController.cs
+++ Assets/Scripts/Ring/RingController.cs
@@ -5,12 +5,16 @@ public class RingController : MonoBehaviour
 
     protected void JumpLeft(Rigidbody rb, float _force)
     {
+        SoundManager.Instance.PlaySFX(SoundType.Jump);
+
         rb.velocity = Vector3.zero;
         rb.AddForce(new Vector3(-1f, 2f, 0f) *  _force, ForceMode.Impulse);
     }
 
     protected void JumpRight(Rigidbody rb, float _force)
     {
+        SoundManager.Instance.PlaySFX(SoundType.Jump);
+
         rb.velocity = Vector3.zero;
         rb.AddForce(new Vector3(1f, 2f, 0f) * _force, ForceMode.Impulse);
     }
