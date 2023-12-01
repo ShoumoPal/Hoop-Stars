6d55f7aa67898e11f22d046338a83c459bca9df4
ShoumoPal <arka.pal21@gmail.com>
2023-12-01 14:05:55 +0530

    Added Observer Pattern and few minor adjustments

--- Assets/Scripts/Goal/GoalController.cs
+++ Assets/Scripts/Goal/GoalController.cs
@@ -1,92 +1,125 @@
+using Bot;
+using UI;
+using Player;
+using Events;
 using UnityEngine;
+using Generics;
 
 // Goal controller class helps to manage the score and communicate with the UI. Also maintains goal logic.
 
-public class GoalController : MonoBehaviour
+namespace Goal
 {
-    private bool _playerCheck1;
-    private bool _playerCheck2;
-    private bool _botCheck1;
-    private bool _botCheck2;
-
-    private int _playerScore;
-    private int _botScore;
-
-    private void Awake()
+    public class GoalController : GenericMonoLazySingleton<GoalController>
     {
-        _playerScore = 0;
-        _botScore = 0;
+        private bool _playerCheck1;
+        private bool _playerCheck2;
+        private bool _botCheck1;
+        private bool _botCheck2;
 
-        _playerCheck1 = false;
-        _playerCheck2 = false;
-        _botCheck1 = false;
-        _botCheck2 = false;
-    }
+        [SerializeField] private int _check1Layer;
+        [SerializeField] private int _check2Layer;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        CheckIfScoredGoal(other);
-    }
+        private int _playerScore;
+        private int _botScore;
 
-    private void OnTriggerExit(Collider other)
-    {
-        CheckExitedGoal(other);
-    }
-
-    private void CheckExitedGoal(Collider other)
-    {
-        if (other.GetComponentInParent<PlayerController>())
+        private void OnEnable()
         {
-            if (other.CompareTag("Check 1"))
-                _playerCheck1 = false;
-            else if (other.CompareTag("Check 2"))
-                _playerCheck2 = false;
+            EventService.Instance.HasPlayerWon += HasPlayerWon;
+            EventService.Instance.HasBotWon += HasBotWon;
         }
-        else if (other.GetComponentInParent<BotController>())
+
+        private void Start()
         {
-            if (other.CompareTag("Check 1"))
-                _botCheck1 = false;
-            else if (other.CompareTag("Check 2"))
-                _botCheck2 = false;
+            _playerScore = 0;
+            _botScore = 0;
+
+            _playerCheck1 = false;
+            _playerCheck2 = false;
+            _botCheck1 = false;
+            _botCheck2 = false;
         }
-    }
 
-    private void CheckIfScoredGoal(Collider other)
-    {
-        if (other.GetComponentInParent<PlayerController>())
+        private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Check 1"))
-                _playerCheck1 = true;
-            else if (other.CompareTag("Check 2"))
-                _playerCheck2 = true;
+            CheckIfScoredGoal(other);
+        }
 
-            if (_playerCheck1 && _playerCheck2)
+        private void OnTriggerExit(Collider other)
+        {
+            CheckExitedGoal(other);
+        }
+
+        private void CheckExitedGoal(Collider other)
+        {
+            if (other.GetComponentInParent<PlayerController>())
             {
-                _playerScore++;
-                UIManager.Instance.AddPlayerScoreAndShow(_playerScore);
+                if (IsInLayerMask(other.gameObject.layer, _check1Layer))
+                    _playerCheck1 = false;
+                else if (IsInLayerMask(other.gameObject.layer, _check2Layer))
+                    _playerCheck2 = false;
             }
-        }
-        if (other.GetComponentInParent<BotController>())
-        {
-            if (other.CompareTag("Check 1"))
-                _botCheck1 = true;
-            else if (other.CompareTag("Check 2"))
-                _botCheck2 = true;
-
-            if (_botCheck1 && _botCheck2)
+            else if (other.GetComponentInParent<BotController>())
             {
-                _botScore++;
-                UIManager.Instance.AddBotScoreAndShow(_botScore);
+                if (IsInLayerMask(other.gameObject.layer, _check1Layer))
+                    _botCheck1 = false;
+                else if (IsInLayerMask(other.gameObject.layer, _check2Layer))
+                    _botCheck2 = false;
             }
         }
 
-        if (_playerScore == 3)
+        private void CheckIfScoredGoal(Collider other)
         {
-            UIManager.Instance.ShowGameOverPanel("player");
+            if (other.GetComponentInParent<PlayerController>())
+            {
+                if (IsInLayerMask(other.gameObject.layer, _check1Layer))
+                    _playerCheck1 = true;
+                else if (IsInLayerMask(other.gameObject.layer, _check2Layer))
+                    _playerCheck2 = true;
+
+                if (_playerCheck1 && _playerCheck2)
+                {
+                    _playerScore++;
+                    UIManager.Instance.AddPlayerScoreAndShow(_playerScore);
+                }
+            }
+            if (other.GetComponentInParent<BotController>())
+            {
+                if (IsInLayerMask(other.gameObject.layer, _check1Layer))
+                    _botCheck1 = true;
+                else if (IsInLayerMask(other.gameObject.layer, _check2Layer))
+                    _botCheck2 = true;
+
+                if (_botCheck1 && _botCheck2)
+                {
+                    _botScore++;
+                    UIManager.Instance.AddBotScoreAndShow(_botScore);
+                }
+            }
+
+            HasPlayerWon();
+            HasBotWon();
         }
-        else if (_botScore == 3)
+
+        private bool HasPlayerWon()
         {
-            UIManager.Instance.ShowGameOverPanel("Bot");
+            return _playerScore == 3;
+        }
+
+        private bool HasBotWon()
+        {
+            return _botScore == 3;
+        }
+
+        private bool IsInLayerMask(int layer, int layerMask)
+        {
+            return (layerMask == layer);
+        }
+
+        private void OnDestroy()
+        {
+            EventService.Instance.HasPlayerWon -= HasPlayerWon;
+            EventService.Instance.HasBotWon -= HasBotWon;
         }
     }
 }
+
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,61 +1,65 @@
 using UnityEngine;
+using Ring;
 
 // PlayerController class inherits from RingController and provides functionality for the Player
 
-public class PlayerController : RingController
+namespace Player
 {
-    [SerializeField] private static PlayerController instance;
-    public static PlayerController Instance { get { return instance; } }
-
-    [SerializeField] private float _force;
-    [SerializeField] private float _boundY;
-    [SerializeField] private Vector3 _tagOffset;
-    [SerializeField] private TextMesh _playerTag;
-
-    private bool _inYBound;
-    private Rigidbody playerRb;
-
-    private void Awake()
+    public class PlayerController : RingController
     {
-        _inYBound = true;
-        instance = this;
-    }
+        [SerializeField] private static PlayerController instance;
+        public static PlayerController Instance { get { return instance; } }
 
-    private void Start()
-    {
-        playerRb = GetComponent<Rigidbody>();
-    }
+        [SerializeField] private float _force;
+        [SerializeField] private float _boundY;
+        [SerializeField] private Vector3 _tagOffset;
+        [SerializeField] private TextMesh _playerTag;
 
-    private void Update()
-    {
-        CheckBound();
-        FollowRingPosition(_playerTag.transform, _tagOffset);
-    }
-    
-    private void CheckBound()
-    {
-        if (transform.position.y > _boundY)
-        {
-            _inYBound = false;
-        }
-        else
+        private bool _inYBound;
+        private Rigidbody playerRb;
+
+        private void Awake()
         {
             _inYBound = true;
+            instance = this;
         }
-    }
-    public void OnJumpLeft()
-    {
-        if (_inYBound)
-        {
-            JumpLeft(playerRb, _force);
-        }
-    }
 
-    public void OnJumpRight() 
-    { 
-        if (_inYBound)
+        private void Start()
         {
-            JumpRight(playerRb, _force);
+            playerRb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            CheckBound();
+            FollowRingPosition(_playerTag.transform, _tagOffset);
+        }
+
+        private void CheckBound()
+        {
+            if (transform.position.y > _boundY)
+            {
+                _inYBound = false;
+            }
+            else
+            {
+                _inYBound = true;
+            }
+        }
+        public void OnJumpLeft()
+        {
+            if (_inYBound)
+            {
+                JumpLeft(playerRb, _force);
+            }
+        }
+
+        public void OnJumpRight()
+        {
+            if (_inYBound)
+            {
+                JumpRight(playerRb, _force);
+            }
         }
     }
 }
